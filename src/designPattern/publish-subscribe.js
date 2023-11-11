2b9b2cbce3a3cc3dabb6c8bceb3c8d00e08810d8
zhouyijun <zhou495248579@hotmail.com>
2023-11-11 20:21:04 +0800

    feat:发布订阅模式

---
+++ src/designPattern/publish-subscribe.js
@@ -0,0 +1,43 @@
+const publish = {
+  events: {},
+  addEvent(key, cb) {
+    if (!this.events[key]) {
+      this.events[key] = [];
+    }
+    this.events[key].push(cb);
+  },
+  removeEvent(key, cb) {
+    const cbs = this.events[key];
+    if (cbs && cbs.length) {
+      if (!cb) {
+        this.events[key] = [];
+        return;
+      }
+      const index = cbs.findIndex((item) => {
+        return item === cb;
+      });
+
+      if (index !== -1) {
+        cbs.splice(index, 1);
+      }
+    }
+  },
+  trigger(key, data) {
+    const cbs = this.events[key] || [];
+    cbs.forEach((item) => {
+      item(data);
+    });
+  },
+};
+const cb1 = (d) => {
+  console.log("motherFuck", d);
+};
+publish.addEvent("fire", cb1);
+
+publish.addEvent("fire", (d) => {
+  console.log("wawawa", d);
+});
+
+publish.trigger("fire", "data");
+publish.removeEvent("fire");
+publish.trigger("fire", "data");

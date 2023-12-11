4312849f30b499b936e25b0fdce0af28a632b134
zhouyijun <zhou495248579@hotmail.com>
2023-12-11 12:51:13 +0800

    feat:canvas clip 图像渲染

--- src/react/canvas/clip/index.tsx
+++ src/react/canvas/clip/index.tsx
@@ -15,7 +15,7 @@ const CanvasBox = styled.div`
   width: 500px;
   height: 500px;
   border: 1px solid rgba(0, 0, 0, 0.1);
-  background: url('${require('./image/mosaic.jpg')}') 100% 100%;
+  background: url("${require("./image/mosaic.jpg")}") 100% 100%;
 
   display: flex;
   justify-content: center;
@@ -70,13 +70,13 @@ let canvasSize = {} as {
   width: number;
   height: number;
 };
-let createURL = "";
+let uploadFileCreateURL = "";
 let initSize = {} as {
   width: number;
   height: number;
   proportion: number;
 };
-let img: HTMLImageElement;
+let uploadImageELement: HTMLImageElement;
 let imgSize = {} as {
   width: number;
   height: number;
@@ -106,11 +106,91 @@ const ClipDemo: FC<Props> = () => {
 
   const [dataUrl, setDataUrl] = useState("");
 
-  function cancelChangeSelect() {}
-  function handleChoiseImg() {}
+  useEffect(() => {
+    init();
+  }, []);
+  function init() {
+    initSize = {
+      width: 500,
+      height: 500,
+      proportion: 1,
+    };
+    ctx = canvasRef.current.getContext("2d");
+    ratio = getPixelRatio(ctx);
+  }
+
+  function drawImage() {
+    const { width: canvasWidth, height: canvasHeight } = canvasSize;
+    const { width: imgWidth, height: imgHeight } = imgSize;
+    console.log(canvasWidth, canvasHeight, imgWidth, imgHeight, imgScale);
+    ctx.save();
+    ctx.drawImage(
+      uploadImageELement,
+      (canvasWidth - imgWidth * imgScale) / 2,
+      (canvasHeight - imgHeight * imgScale) / 2,
+      imgWidth * imgScale,
+      imgHeight * imgScale,
+    );
+    ctx.restore();
+  }
+
+  function calcCanvasSize() {
+    const { width: imageWidth, height: imageHeight } = imgSize;
+    const canvasRenderWidth = Math.min(imageWidth * imgScale, initSize.width);
+    const canvasRenderHeight = Math.min(
+      imageHeight * imgScale,
+      initSize.height,
+    );
+    canvasRef.current.style.width = `${canvasRenderWidth}px`;
+    canvasRef.current.style.height = `${canvasRenderHeight}px`;
+    canvasRef.current.width = ratio * canvasRenderWidth;
+    canvasRef.current.height = ratio * canvasRenderHeight;
+    ctx.scale(ratio, ratio);
+    canvasSize = {
+      width: canvasRenderWidth,
+      height: canvasRenderHeight,
+    };
+    mousePosi = []; // 每次重置canvas清空
+  }
+  function initImage() {
+    imgSize = {
+      width: uploadImageELement.width,
+      height: uploadImageELement.height,
+    };
+    const propotion = uploadImageELement.width / uploadImageELement.height;
+    if (
+      uploadImageELement.width < initSize.width &&
+      uploadImageELement.height < initSize.height
+    ) {
+      imgScale = 1;
+      return;
+    }
+    if (propotion > initSize.proportion) {
+      imgScale = initSize.width / uploadImageELement.width;
+    } else if (propotion < initSize.proportion) {
+      imgScale = initSize.height / uploadImageELement.height;
+    }
+  }
+
+  function handleChoiseImg(event) {
+    console.log(event);
+    if (uploadFileCreateURL) {
+      window.URL.revokeObjectURL(uploadFileCreateURL); // 释放先前建立的URL
+    }
+    uploadFileCreateURL = window.URL.createObjectURL(inputRef.current.files[0]);
+    uploadImageELement = new Image();
+    uploadImageELement.src = uploadFileCreateURL;
+    uploadImageELement.onload = function () {
+      initImage();
+      calcCanvasSize();
+      drawImage();
+    };
+  }
 
   function mouseMove() {}
+
   function mouseDown() {}
+  function cancelChangeSelect() {}
   return (
     <>
       <Layout

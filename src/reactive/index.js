eadafddc785b382fdd3c37e75679d4eceab2f606
周义竣 <zhouyijun@chagee.com>
2026-02-28 16:24:47 +0800

    feat(reactive): implement ref and computed APIs for reactive state management
    - Added ref function to create reactive references for primitive values.
    - Introduced computed function for lazy evaluation of derived state based on a getter.
    - Updated effect function to return the effectFn for external usage, enhancing flexibility in reactive programming.

--- src/reactive/index.js
+++ src/reactive/index.js
@@ -65,7 +65,7 @@ export function effect(fn, options = {}) {
     effectStack.push(activeEffect);
 
     try {
-      fn();
+      return fn();
     } finally {
       effectStack.pop();
       activeEffect = effectStack[effectStack.length - 1] || null;
@@ -73,13 +73,58 @@ export function effect(fn, options = {}) {
   };
   effectFn.deps = [];
   effectFn.options = options;
-  if (!options.delay) {
+  if (!options.lazy) {
     effectFn();
   }
+  return effectFn;
 }
 
 export function cleanup(effect) {
   effect.deps.forEach((dep) => {
     dep.delete(effect);
   });
+}
+
+export function ref(value) {
+  const wrapper = {
+    _value: value,
+    get value() {
+      track(wrapper, "value");
+      return this._value;
+    },
+    set value(val) {
+      if (val !== this._value) {
+        this._value = val;
+
+        trigger(wrapper, "value");
+      }
+    },
+  };
+  return wrapper;
+}
+export function computed(getter) {
+  let dirty = true;
+  let value = null;
+  let effectFn = effect(getter, {
+    schedule: () => {
+      if (dirty) {
+        return;
+      }
+      dirty = true;
+      trigger(wrapper, "value");
+    },
+    lazy: true,
+  });
+  const wrapper = {
+    get value() {
+      track(wrapper, "value");
+      if (dirty) {
+        value = effectFn();
+        dirty = false;
+      }
+
+      return value;
+    },
+  };
+  return wrapper;
 }
